c17df2e99e3be764a7ce1f976ad3e55200adf1b2
Soin Na <carrotcold@gmail.com>
2020-11-27 17:23:08 +0900

    :bento: update speech game's phrases

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ const userController = require('./controllers/users.controller');
 router.post(ROUTE.LOGIN.GOOGLE, userController.googleLogin);
 router.post(ROUTE.LOGIN.TOKEN, userController.tokenLogin);
 router.post(ROUTE.USER.LOGOUT, verifyToken, userController.logoutUser);
-router.get(ROUTE.USER.FRIENDS, verifyToken, userController.getFriendList);
+router.get(ROUTE.USER.FRIENDS.ROOT, verifyToken, userController.getFriendList);
 router.get(ROUTE.USER.FRIENDS.REQUEST, verifyToken, userController.getFriendRequestList);
 router.post(ROUTE.USER.FRIENDS.REQUEST, verifyToken, userController.requestFriend);
 router.put(ROUTE.USER.FRIENDS.REQUEST, verifyToken, userController.responseFriendRequest);
